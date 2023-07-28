b05236f46053393abd44ad9951ff06035402d712
j-augusto-oliveira-sant <j.augusto.oliveira.sant@gmail.com>
2023-07-27 21:19:43 -0300

    formatacao e templates analise lexica

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 # from pydantic import BaseModel
@@ -14,10 +15,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+@app.get("/", response_class=HTMLResponse)
+def show_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 @app.post("/analise-lexica")
-def analise_lexica(codigo: str = Form(..., max_length=250)):
+def analise_lexica(request: Request, codigo: str = Form(..., max_length=250)):
     print(f"Enviado: {codigo}")
     return templates.TemplateResponse(
         "result.html",
-        {"result": {"tokens": analisador_lexico(codigo)}},
+        {"result": {"tokens": analisador_lexico(codigo)}, "request": request},
     )
